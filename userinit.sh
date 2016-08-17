3904e1f7bee489347c3e57a0671a7c3af2b0f95e
Alex Raschi <raschi.alex@gmail.com>
2016-08-17 18:18:19 +0200

    update userinit.sh
    seems that cfq perform best... vlc with cfq is able to play some
    videos better than before for me

--- userinit.sh
+++ userinit.sh
@@ -2,12 +2,6 @@
 
 echo "100000" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
 echo "intelliactive" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-echo "0" > /sys/module/mali/parameters/mali_debug_level
-echo "0" > /sys/module/ump/parameters/ump_debug_level
-echo "deadline" > /sys/block/mmcblk0/queue/scheduler
-echo "0" > /sys/block/mmcblk0/queue/iostats
-echo "noop" > /sys/block/mmcblk1/queue/scheduler
-echo "0" > /sys/block/mmcblk1/queue/iostats
 echo "1" > /sys/devices/virtual/sec/led/led_fade
 echo "0" > /sys/devices/platform/samsung-battery/sdp_input_curr
 echo "0" > /sys/devices/platform/samsung-battery/sdp_chrg_curr
@@ -19,8 +13,6 @@ echo "1" > /sys/devices/virtual/misc/boeffla_sound/dac_direct
 echo "1" > /sys/devices/virtual/misc/boeffla_sound/dac_oversampling
 echo "1" > /sys/devices/virtual/misc/boeffla_sound/fll_tuning
 echo "1" > /sys/devices/virtual/misc/boeffla_sound/speaker_tuning
-echo "51 51" > /sys/devices/virtual/misc/boeffla_sound/headphone_volume
-echo "51 51" > /sys/devices/virtual/misc/boeffla_sound/speaker_volume
 echo "1" > /sys/devices/virtual/misc/boeffla_sound/privacy_mode
 echo "0" > /sys/devices/virtual/camera/rear/camera_speaker_enabled
 
